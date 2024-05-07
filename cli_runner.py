de3da84b2ac0f92404afd3d34d18849e443a664b
Frankium <tis7bfrankie@gmail.com>
2024-05-08 03:38:20 +1000

    fix stdin parsing

--- cli_runner.py
+++ cli_runner.py
@@ -13,21 +13,21 @@ def exec_basic_interface(interface_name: str, *args: str):
     if interface_name not in BASIC_INTERFACE:
       raise EelExcCommandNotFound(f"{interface_name} is not a valid command")
 
-    lst_args = list(args)
     results_arg: list[EelTypeBase] = []
     interface = BASIC_INTERFACE[interface_name]
 
-    for i, arg in enumerate(args):
+    for arg in args:
       if arg and (arg[:2] == (PIPE_IN_HOLDER * 2)):
-        lst_args[i] = arg[1:]
+        arg = arg[1:]
       
       elif arg == PIPE_IN_HOLDER:
-        lst_args[i] = read_to_eof()
+        arg = read_to_eof()
 
       try:
         results_arg.append(decode_literal_or_repr(arg))
       except EelExcNotAnEelRepr:
         results_arg.append(EelTypeString(arg))
+
 
     result: EelTypeBase = interface(*results_arg)
     stdout.write(result.eel_encode_repr())
